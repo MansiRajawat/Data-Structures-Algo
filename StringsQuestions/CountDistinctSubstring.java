fb4d20c77f854d4733dbc4befbd8c5a17a6db7d8
dhruvajyoti1997 <dhruvajyotipramanik080@gmail.com>
2025-05-11 14:13:17 +0530

    count distinct substring in a string

---
+++ StringsQuestions/CountDistinctSubstring.java
@@ -0,0 +1,53 @@
+package StringsQuestions;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CountDistinctSubstring {
+    public static void main(String[] args) {
+        //we need to count all the distinct substring in a string
+        String str = "abcd";
+
+        //a, b, c, d, ab, abc, abcd, bc, bcd, cd
+
+        //we should return the size or the count
+
+        //we need a set interface to store the elements
+
+      int result = findAllDistinctSubstringsApproach1(str);
+        System.out.println("approach 1 result --> "+result);
+
+        int resultApproach2 = findAllDistinctSubstringsApproach2(str);
+        System.out.println("approach 2 result --> "+ resultApproach2);
+
+    }
+
+    private static int findAllDistinctSubstringsApproach2(String str) {
+        //this approach using the internal substring function
+        Set<String> set = new HashSet<>();
+
+        for(int i=0; i < str.length(); i++){
+            for(int j = i+1; j <= str.length(); j++){
+                set.add(str.substring(i,j));
+            }
+        }
+        return set.size();
+    }
+
+    private static int findAllDistinctSubstringsApproach1(String str) {
+        //we need to calculate the substring and store in a set
+        Set<String> set = new HashSet<>();
+
+        for(int i=0; i < str.length(); i++){
+            StringBuilder sb = new StringBuilder();
+
+            for(int j=i; j < str.length(); j++){
+                sb.append(str.charAt(j));
+                set.add(sb.toString());
+            }
+        }
+       return set.size();
+    }
+
+
+}

26ed5d905ea71a23e826fe93cda246185b3e4101
dhruvajyoti1997 <dhruvajyotipramanik080@gmail.com>
2025-12-11 07:38:52 +0530

    correction of logic

--- ArraysQuestions/MinimumSubArraySum.java
+++ ArraysQuestions/MinimumSubArraySum.java
@@ -3,23 +3,24 @@ package ArraysQuestions;
 public class MinimumSubArraySum {
     public static void main(String[] args) {
         int [] arr2= {1, 3, 2, 4};
+        minimumSubArraySum(arr2);
     }
     private static void minimumSubArraySum(int[] arr2) {
-        int sum =0;
 
+        int  result = Integer.MAX_VALUE;
         for(int i=0; i < arr2.length; i++){
             for(int j =i; j < arr2.length; j++){
 
-                int  min = arr2[i];
+                int sum =0;
 
                 for(int k=i; k <=j ; k++){
-                    min = Math.min(min, arr2[k]);
+                   sum = sum + arr2[k];
                 }
 
-                sum = sum + min;
+                result = Math.min(result, sum);
             }
         }
 
-        System.out.println("minimum sub array sum " +sum);
+        System.out.println("minimum sub array sum " +result);
     }
 }

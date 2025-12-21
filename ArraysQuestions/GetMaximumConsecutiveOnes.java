141b82e2a2b9ed49391aa12247f609bda8632408
dhruvajyoti1997 <dhruvajyotipramanik080@gmail.com>
2025-12-21 13:51:52 +0530

    logic correction

--- ArraysQuestions/GetMaximumConsecutiveOnes.java
+++ ArraysQuestions/GetMaximumConsecutiveOnes.java
@@ -2,7 +2,7 @@ package ArraysQuestions;
 
 public class GetMaximumConsecutiveOnes {
     public static void main(String[] args) {
-        int[] nums = {1, 1, 0, 0, 1, 1, 1, 0};
+        int[] nums = {1,0,1,1,0,1};
         getMaximumConsecutiveOnes(nums);
     }
     private static void getMaximumConsecutiveOnes(int[] nums) {
@@ -10,14 +10,13 @@ public class GetMaximumConsecutiveOnes {
         int tempCount =0;
 
         for(int i=0; i < nums.length; i++){
-            if(nums[i] == 1){
+            if(nums[i] == 1) {
                 tempCount++;
-            }
-            count = tempCount;
-
-            if(nums[i] == 0){
+            } else if(nums[i] == 0){
                 tempCount =0;
             }
+
+            count = Math.max(count, tempCount);
         }
 
         System.out.println(count);

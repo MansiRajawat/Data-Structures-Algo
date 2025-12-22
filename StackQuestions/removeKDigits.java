6ac1069ddcb45a7858cedd9ccd3e5f3f0a0894f0
dhruvajyoti1997 <dhruvajyotipramanik080@gmail.com>
2025-12-22 12:15:04 +0530

    logic correction

--- StackQuestions/removeKDigits.java
+++ StackQuestions/removeKDigits.java
@@ -6,38 +6,33 @@ public class removeKDigits {
     public static void main(String[] args) {
         String num = "1432219";
         int k = 3;
+        System.out.println(removeKDigit(num, k));
+    }
+    public static String removeKDigit(String num, int k) {
         //the idea of using stack is to maintain the track of digits in monotnic order
         //first use the for loop and using while loop check below conditions
         //1. if the stack is not empty && k >0 && stack top element is greater than the character
         //then remove the element from stack and reduce the k value
         //2. else push the element in stack.
-        removeKDigits(num, k);
-    }
-    private static void removeKDigits(String num, int k) {
-
         Stack<Character> st = new Stack<>();
-
+        StringBuilder sb = new StringBuilder();
         for(char ch : num.toCharArray()){
 
-            while( !st.isEmpty() && k >0 && st.peek() > ch){
+            while(!st.isEmpty() && k >0 && st.peek() > ch){
                 st.pop();
                 k--;
             }
+            st.push(ch);
+        }
 
-            if(ch != '0'){
-                st.push(ch);
-            }
+        for(char c: st){
+            sb.append(c);
         }
-//the below case is it handle when we have elemnets like 12345 , stack will store all the elements in that case k is not at all 0.
-        while(k > 0 && !st.isEmpty()){
-            st.pop();
-            k--;
+        while (sb.length() > 1 && sb.charAt(0) == '0') {
+            sb.deleteCharAt(0);
         }
-//if the number length and the k value is same then we should return 0.
-        if( k  == num.length()){
-            System.out.println("0");
-        } else {
-            System.out.println( "values   "+st.toString());
-        }
+
+        return sb.isEmpty() ? "0" : sb.toString();
+
     }
 }

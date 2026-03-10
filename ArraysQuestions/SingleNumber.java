8d532fb2ded9d94acac9a5c106b10d2df3281bf6
dhruvajyoti1997 <dhruvajyotipramanik080@gmail.com>
2026-03-10 08:33:37 +0530

    leetcode easy question

--- ArraysQuestions/SingleNumber.java
+++ ArraysQuestions/SingleNumber.java
@@ -1,7 +1,27 @@
 package ArraysQuestions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SingleNumber {
     public static void main(String[] args) {
+        int[] nums = {1,2,1,1};
+        System.out.println(singleNumber(nums));
+    }
 
+    public static int singleNumber(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0; i < nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+        }
+
+        for(Map.Entry<Integer, Integer> val : map.entrySet()){
+            if(val.getValue() == 1){
+                return val.getKey();
+            }
+        }
+
+        return 0;
     }
 }

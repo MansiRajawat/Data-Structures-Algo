d69f640c0396316567908f67359b1594f5951f96
dhruvajyoti1997 <dhruvajyotipramanik080@gmail.com>
2026-03-16 22:15:14 +0530

    logic correction

--- StackQuestions/ValidParanthesis.java
+++ StackQuestions/ValidParanthesis.java
@@ -29,7 +29,6 @@ public class ValidParanthesis {
                 if (ch == '}' && top != '{') return false;
                 if (ch == ']' && top != '[') return false;
 
-                stack.pop();
             }
 
 
